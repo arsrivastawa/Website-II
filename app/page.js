5e3be378c4e416042533b51e0d1ec57f7b9d90c2
Amaan Hussain <amaanhussain983@gmail.com>
2024-04-12 16:15:16 +0530

    Navbar done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import CookingAnimation from "@/components/CookingAnimation/CookingAnimation";
+import Navbar from "@/components/CookingAnimation/Navbar";
 
 export default function Home() {
   return (
     <>
-      <CookingAnimation />
+      {/* <CookingAnimation /> */}
+      <Navbar/>
     </>
   );
 }
