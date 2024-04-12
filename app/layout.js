2dce9f2da522348971a2d507c01329fa984ff64b
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-04-12 14:39:29 +0530

    Changed: Folder Structure

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`bg-[#0c0c0c] ${inter.className}`}>{children}</body>
+      <body className={`bg-[#0c0c0c] ${inter.className}`}>
+        <div className="body-first-child" />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/CookingAnimation/CookingAnimation.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function CookingAnimation() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div className="w-full flex justify-center">
+        <img
+          className="w-[50%] sm:w-[30%] lg:w-[20%] xl:w-[15%]"
+          src="../assets/cooking.gif"
+        />
+      </div>
+      <div className="text-2xl sm:text-4xl lg:text-6xl xl:text-7xl text-center font-squid font-normal text-white">
+        Something Is <br />
+        cooking...
+      </div>
+      {/* <div className="text-7xl font-squid font-normal text-white">
+        HacK-O-VErSe
+      </div> */}
+    </div>
+  );
+}
+
+export default CookingAnimation;
