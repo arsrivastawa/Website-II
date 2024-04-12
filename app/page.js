cd5adf85a88e6e10dfa19068020a3d5c288bd218
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-04-12 10:01:22 +0530

    Added: Cooking GIF and text

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className=" flex items-center justify-center h-screen ">
-      <div className="">Hello</div>
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div className="w-full flex justify-center">
+        <img
+          className="w-[50%] sm:w-[30%] lg:w-[20%] xl:w-[15%]"
+          src="../assets/cooking.gif"
+        />
+      </div>
+      <div className="text-2xl sm:text-4xl lg:text-6xl xl:text-7xl text-center font-squid font-normal text-white">
+        Something Is <br />
+        cooking...
+      </div>
+      {/* <div className="text-7xl font-squid font-normal text-white">
+        HacK-O-VErSe
+      </div> */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        squid: ["GameOfSquids", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
