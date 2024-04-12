06f8d3459727aef4535e17e72b9e2619a164042b
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-04-12 16:20:35 +0530

    Added: Hero Heading

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import CookingAnimation from "@/components/CookingAnimation/CookingAnimation";
+import HeroHeading from "@/components/CookingAnimation/HeroHeading";
 
 export default function Home() {
   return (
     <>
       <CookingAnimation />
+      <HeroHeading />
     </>
   );
 }
---
+++ components/CookingAnimation/HeroHeading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeroHeading() {
+  return (
+    <div className="w-full absolute top-[10%] opacity-15 left-1/2 -translate-x-1/2">
+      <div>
+        <h1 className="w-full text-center stroke-text text-9xl font-squid font-normal">
+          HacK-O-VErSe
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default HeroHeading;
